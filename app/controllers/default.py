e5dfca98a3ad4429ace47693d6374bf6f007159c
Gustavo Comerlatto <gustcomer@gmail.com>
2019-09-11 09:06:37 -0300

    O codigo depois de metade do curso da Julia Rizza

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,5 +1,23 @@
-from app import app
+from flask import render_template
+from app import app, db
+
+from app.models.tables import User
+
+@app.route('/home')
+@app.route('/index')
+@app.route('/')
+def home():
+	return "Hello world!"
 
 @app.route('/<name>')
 def index(name):
-	return '<h1>Hello {}!<h1>'.format(name)
+	return '<h1>Hellow {}!<h1>'.format(name)
+
+@app.route("/teste/<info>")
+@app.route("/teste", defaults={"info":None})
+def teste(info):
+	i = User("maria", "1234", "Julia Rizza",
+		"maria@gmail.com")
+	db.session.add(i)
+	db.session.commit()
+	return 'ok'
